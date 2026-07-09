043e396a00a2ea875a6b39e48682b7e256ecedf9
anupamaqa0812 <anupama.singh50@yahoo.com>
2026-07-09 15:23:36 -0400

    first commit

---
+++ src/test/java/com/test/LoginTest.java
@@ -0,0 +1,22 @@
+package com.test;
+
+import org.testng.annotations.Test;
+
+import com.base.BaseTest;
+import com.driver.DriverFactory;
+import com.pages.LoginPage;
+import com.properties.TestDataLoader;
+
+public class LoginTest extends BaseTest {
+
+	@Test
+	public void loginWithValidUserNameAndValidPassword() {
+
+		LoginPage loginPage = new LoginPage();
+
+		loginPage.loginToApplication(TestDataLoader.getInstance().getUserName(),
+				TestDataLoader.getInstance().getPassword());
+
+	}
+
+}
